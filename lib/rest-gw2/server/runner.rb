c49cf7910d6c6df4ad6d77d5c1f50d8afc21d6c5
Lin Jen-Shin <godfat@godfat.org>
2015-12-10 11:16:28 +0800

    so that we could pick which server to use

--- lib/rest-gw2/server/runner.rb
+++ lib/rest-gw2/server/runner.rb
@@ -5,36 +5,47 @@ module RestGW2::Runner
   module_function
   def options
     @options ||=
-      [['Options:'       , ''                            ],
-       ['-o, --host HOST', 'Use HOST (default: 0.0.0.0)' ],
-       ['-p, --port PORT', 'Use PORT (default: 8080)'    ],
-       ['-c, --configru' , 'Print where the config.ru is'],
-       ['-h, --help'     , 'Print this message'          ],
-       ['-v, --version'  , 'Print the version'           ]]
+      [['Options:'           , ''                             ],
+       ['-o, --host HOST'    , 'Use HOST (default: 0.0.0.0)'  ],
+       ['-p, --port PORT'    , 'Use PORT (default: 8080)'     ],
+       ['-s, --server SERVER', 'Use SERVER (default: webrick)'],
+       ['-c, --configru'     , 'Print where the config.ru is' ],
+       ['-h, --help'         , 'Print this message'           ],
+       ['-v, --version'      , 'Print the version'            ]]
+  end
+
+  def root
+    File.expand_path("#{__dir__}/../../../")
   end
 
   def config_ru_path
-    File.expand_path("#{__dir__}/../../../config.ru")
+    "#{root}/config.ru"
   end
 
   def run argv=ARGV
-    unused, host, port = parse(argv)
+    unused, host, port, server = parse(argv)
     warn("Unused arguments: #{unused.inspect}") unless unused.empty?
     require 'rack'
-    Rack::Handler::WEBrick.run(Rack::Builder.new{
+    Rack::Handler.get(server).run(Rack::Builder.new{
       eval(File.read(RestGW2::Runner.config_ru_path))
-    }.to_app, :Host => host, :Port => port)
+    }.to_app, :Host => host, :Port => port, :config => root)
   end
 
   def parse argv
-    unused, host, port = [], nil, nil
+    unused, host, port, server = [], '0.0.0.0', 8080, 'webrick'
     until argv.empty?
       case arg = argv.shift
       when /^-o=?(.+)?/, /^--host=?(.+)?/
         host = $1 || argv.shift
+        missing_arg('host') unless host
 
       when /^-p=?(.+)?/, /^--port=?(.+)?/
         port = $1 || argv.shift
+        missing_arg('port') unless port
+
+      when /^-s=?(.+)?/, /^--server=?(.+)?/
+        server = $1 || argv.shift
+        missing_arg('server') unless server
 
       when /^-c/, '--configru'
         puts(config_ru_path)
@@ -54,11 +65,16 @@ module RestGW2::Runner
       end
     end
 
-    [unused, host, port]
+    [unused, host, port, server]
   end
 
   def parse_next argv, arg
     argv.unshift("-#{arg[2..-1]}") if arg.size > 2
+  end
+
+  def missing_arg arg
+    warn("Missing argument: #{arg}")
+    exit(1)
   end
 
   def help
