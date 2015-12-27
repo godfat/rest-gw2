cc9613ec101cc8d7cb36066e41f899ee33a2d7ba
Lin Jen-Shin <godfat@godfat.org>
2015-12-27 18:00:11 +0800

    show 200 records for one transaction page

--- lib/rest-gw2/client.rb
+++ lib/rest-gw2/client.rb
@@ -78,9 +78,9 @@ module RestGW2
 
     # https://wiki.guildwars2.com/wiki/API:2/items
     # https://wiki.guildwars2.com/wiki/API:2/commerce/prices
-    def with_item_detail path, opts={}, &block
+    def with_item_detail path, query={}, opts={}, &block
       block ||= :itself.to_proc
-      items = block.call(get(path, {}, opts))
+      items = block.call(get(path, query, opts))
       ids   = items.map{ |i| i && i['id'] }
 
       detail = ids.compact.each_slice(100).map do |slice|
@@ -96,7 +96,8 @@ module RestGW2
 
     # https://wiki.guildwars2.com/wiki/API:2/commerce/transactions
     def transactions_with_detail path, opts={}
-      with_item_detail("v2/commerce/transactions/#{path}", opts) do |trans|
+      with_item_detail("v2/commerce/transactions/#{path}",
+                       {:page_size => 200}, opts) do |trans|
         trans.map do |t|
           t.merge('id' => t['item_id'], 'count' => t['quantity'])
         end
